2c36c79338ee50bec20206463ba442d0a913f76a
Taazim Temirova <tezi.temirova@gmail.com>
2020-04-01 21:28:07 +0600

    created User object

---
+++ 3_57/script.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+class User {
+  constructor(id, name, email, isAuthorised) {
+    this.id = id,
+    this.name = name,
+    this.email = email,
+    this.isAuthorised = isAuthorised;
+  }
+};
+
